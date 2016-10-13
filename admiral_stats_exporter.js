f511e00c7069e750b911860b4cc5fb67c30b8794
Masahiro Yoshizawa <muziyoshiz@gmail.com>
2016-10-14 01:48:17 +0900

    JavaScript 本体を @sophiarcp さんの最新版に置き換え

--- admiral_stats_exporter.js
+++ admiral_stats_exporter.js
@@ -1,63 +1,43 @@
-// Base URL
-var BASE_URL = 'https://kancolle-arcade.net/ac/api/';
+var date = new Date();
+var ymdhms = [date.getFullYear(),
+  ('0' + (date.getMonth() + 1)).slice(-2),
+  ('0' + date.getDate()).slice(-2),
+  '_',
+  ('0' + date.getHours()).slice(-2),
+  ('0' + date.getMinutes()).slice(-2),
+  ('0' + date.getSeconds()).slice(-2)].join('');
 
-// API URLs
-var API_URLS = [
-  'Personal/basicInfo',
-  'Area/captureInfo',
-  'TcBook/info',
-  'EquipBook/info',
-  'Campaign/history',
-  'Campaign/info',
-  'Campaign/present',
-  // From REVISION 2 (2016-06-30)
-  'CharacterList/info',
-  'EquipList/info',
-  // From 2016-07-26
-  'Quest/info'
-]
-
-// timestamp format: %Y%m%d_%H%M%S
-var now = new Date,
-  timestamp = [
-    now.getFullYear(),
-    ('0' + (now.getMonth() + 1)).slice(-2),
-    ('0' + now.getDate()).slice(-2),
-    '_',
-    ('0' + now.getHours()).slice(-2),
-    ('0' + now.getMinutes()).slice(-2),
-    ('0' + now.getSeconds()).slice(-2)].join(''),
-  fileNameSuffix = '_' + timestamp + '.json'
-
-API_URLS.forEach(
-  function (url) {
-    var request = new XMLHttpRequest,
-      fileName = url.replace('/', '_') + fileNameSuffix;
-    request.open('GET', BASE_URL + url);
-    request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-    request.responseType = 'blob';
-
-    request.onload = function () {
-      if (request.status === 200) {
-        var blob = new Blob([request.response]);
-        if (window.navigator.msSaveBlob) {
-          // Windows の場合は msSaveBlob でファイルに保存
-          window.navigator.msSaveBlob(blob, fileName);
-        } else {
-          // Windows 以外の場合は HTML5 の download 属性を用いてファイル名を指定
-          var objectUrl = (window.URL || window.webkitURL).createObjectURL(blob),
-            a = document.createElement('a');
-          document.body.appendChild(a);
-          a.download = url.replace('/', '_') + '_' + timestamp + '.json';
-          a.href = objectUrl;
-          a.click();
-          document.body.removeChild(a);
-          console.log(objectUrl)
-        }
+var api_base = 'https://kancolle-arcade.net/ac/api/';
+var infoarray = ['Personal/basicInfo', 'Area/captureInfo', 'TcBook/info', 'EquipBook/info', 'Campaign/history', 'Campaign/info', 'Campaign/present', 'CharacterList/info', 'EquipList/info', 'Quest/info'];
+/*    var infoarray = ['Personal/basicInfo', 'TcBook/info', 'CharacterList/info'];
+ */
+infoarray.forEach(function (u) {
+  var xhr = new XMLHttpRequest();
+  var fname = u.replace('/', '_') + '_' + ymdhms + '.json';
+  xhr.open('GET', api_base + u);
+  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+  xhr.responseType = 'blob';
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      /* success */
+      var blob = new Blob([xhr.response]);
+      if (window.navigator.msSaveBlob) {
+        window.navigator.msSaveBlob(blob, fname);
       } else {
-        console.log(request.responseText)
+        var url = window.URL || window.webkitURL;
+        var blobUrl = url.createObjectURL(blob);
+        var a = document.createElement('a');
+        document.body.appendChild(a);
+        a.download = u.replace('/', '_') + '_' + ymdhms + '.json';
+        a.href = blobUrl;
+        a.click();
+        document.body.removeChild(a);
+        console.log(blobUrl);
       }
-    };
-    request.send();
-  }
-)
+    } else {
+      /* error */
+      /*console.log(xhr.responseText);*/
+    }
+  };
+  xhr.send();
+});
