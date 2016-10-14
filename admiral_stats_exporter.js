ff6d555254ff3e698555ec21b870d8057c63eb4a
Masahiro Yoshizawa <muziyoshiz@gmail.com>
2016-10-14 22:57:43 +0900

    ログイン状態チェックの改善と、エラーメッセージの追加

--- admiral_stats_exporter.js
+++ admiral_stats_exporter.js
@@ -1,5 +1,8 @@
 (function () {
+  // エクスポートの実行時刻
   var date = new Date();
+
+  // エクスポートしたファイルに付与するタイムスタンプ
   var ymdhms = [date.getFullYear(),
     ('0' + (date.getMonth() + 1)).slice(-2),
     ('0' + date.getDate()).slice(-2),
@@ -8,46 +11,54 @@
     ('0' + date.getMinutes()).slice(-2),
     ('0' + date.getSeconds()).slice(-2)].join('');
 
-  var base_addr = 'https://kancolle-arcade.net/ac/';
-  var api_base = base_addr + 'api/';
-  var uri_check = base_addr + '#/area';
-  var infoarray = ['Personal/basicInfo', 'Area/captureInfo', 'TcBook/info', 'EquipBook/info', 'Campaign/history', 'Campaign/info', 'Campaign/present', 'CharacterList/info', 'EquipList/info', 'Quest/info'];
-  /*    var infoarray = ['Personal/basicInfo', 'TcBook/info', 'CharacterList/info'];
-   */
-  if (window.location.href.match(new RegExp('^' + uri_check))) {
-    /*console.log('OK');*/
-    infoarray.forEach(function (u) {
-      var xhr = new XMLHttpRequest();
-      var fname = u.replace('/', '_') + '_' + ymdhms + '.json';
-      xhr.open('GET', api_base + u);
-      xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-      xhr.responseType = 'blob';
-      xhr.onload = function () {
-        if (xhr.status === 200) {
-          /* success */
-          var blob = new Blob([xhr.response]);
-          if (window.navigator.msSaveBlob) {
-            window.navigator.msSaveBlob(blob, fname);
-          } else {
-            var url = window.URL || window.webkitURL;
-            var blobUrl = url.createObjectURL(blob);
-            var a = document.createElement('a');
-            document.body.appendChild(a);
-            a.download = u.replace('/', '_') + '_' + ymdhms + '.json';
-            a.href = blobUrl;
-            a.click();
-            document.body.removeChild(a);
-            console.log(blobUrl);
-          }
-        } else {
-          /* error */
-          /*console.log(xhr.responseText);*/
-        }
-      };
-      xhr.send();
-    });
-  } else {
-    /*console.log('NG');*/
+  // ログイン認証が必要な URL にマッチする正規表現
+  var authorizedUrls = new RegExp('^https://kancolle-arcade\.net/ac/#/(area|list|pictureBook)');
+
+  // API のベースとなる URL
+  var apiUrl = 'https://kancolle-arcade.net/ac/api/';
+
+  // プレイデータの URL の末尾（Admiral Stats がインポートできるプレイデータのみ）
+  var dataTypes = ['Personal/basicInfo', 'TcBook/info', 'CharacterList/info'];
+
+  // プレイデータの URL の末尾（現在取得できるすべてのプレイデータ）
+  // var dataTypes = ['Personal/basicInfo', 'Area/captureInfo', 'TcBook/info', 'EquipBook/info', 'Campaign/history', 'Campaign/info', 'Campaign/present', 'CharacterList/info', 'EquipList/info', 'Quest/info'];
+
+  // ログイン認証が必要な URL にいない場合は、アラートを出して終了
+  if (!window.location.href.match(authorizedUrls)) {
+    alert('プレイヤーズサイトにログインした直後の画面で実行してください。');
     return;
   }
+
+  dataTypes.forEach(function (dataType) {
+    var xhr = new XMLHttpRequest();
+    var fname = dataType.replace('/', '_') + '_' + ymdhms + '.json';
+    xhr.open('GET', apiUrl + dataType);
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.responseType = 'blob';
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        /* success */
+        var blob = new Blob([xhr.response]);
+        if (window.navigator.msSaveBlob) {
+          window.navigator.msSaveBlob(blob, fname);
+        } else {
+          var url = window.URL || window.webkitURL;
+          var blobUrl = url.createObjectURL(blob);
+          var a = document.createElement('a');
+          document.body.appendChild(a);
+          a.download = dataType.replace('/', '_') + '_' + ymdhms + '.json';
+          a.href = blobUrl;
+          a.click();
+          document.body.removeChild(a);
+        }
+      } else {
+        /* error */
+        // 同じエラーが何回も表示されるのを防ぐために、URL の末尾が dataType[0] に一致するときだけエラーメッセージを表示する
+        if (dataType === dataTypes[0]) {
+          alert('接続に失敗しました。プレイヤーズサイトに再ログインしてから実行してください。(status code = ' + xhr.status + ')');
+        }
+      }
+    };
+    xhr.send();
+  });
 })();
