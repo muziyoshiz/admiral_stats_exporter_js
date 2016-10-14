dbe321e5006900e39ddf3f5a9cd7dc6974f067c9
Masahiro Yoshizawa <muziyoshiz@gmail.com>
2016-10-14 21:57:16 +0900

    最新版の登録し直し

--- admiral_stats_exporter.js
+++ admiral_stats_exporter.js
@@ -1,43 +1,53 @@
-var date = new Date();
-var ymdhms = [date.getFullYear(),
-  ('0' + (date.getMonth() + 1)).slice(-2),
-  ('0' + date.getDate()).slice(-2),
-  '_',
-  ('0' + date.getHours()).slice(-2),
-  ('0' + date.getMinutes()).slice(-2),
-  ('0' + date.getSeconds()).slice(-2)].join('');
+(function () {
+  var date = new Date();
+  var ymdhms = [date.getFullYear(),
+    ('0' + (date.getMonth() + 1)).slice(-2),
+    ('0' + date.getDate()).slice(-2),
+    '_',
+    ('0' + date.getHours()).slice(-2),
+    ('0' + date.getMinutes()).slice(-2),
+    ('0' + date.getSeconds()).slice(-2)].join('');
 
-var api_base = 'https://kancolle-arcade.net/ac/api/';
-var infoarray = ['Personal/basicInfo', 'Area/captureInfo', 'TcBook/info', 'EquipBook/info', 'Campaign/history', 'Campaign/info', 'Campaign/present', 'CharacterList/info', 'EquipList/info', 'Quest/info'];
-/*    var infoarray = ['Personal/basicInfo', 'TcBook/info', 'CharacterList/info'];
- */
-infoarray.forEach(function (u) {
-  var xhr = new XMLHttpRequest();
-  var fname = u.replace('/', '_') + '_' + ymdhms + '.json';
-  xhr.open('GET', api_base + u);
-  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-  xhr.responseType = 'blob';
-  xhr.onload = function () {
-    if (xhr.status === 200) {
-      /* success */
-      var blob = new Blob([xhr.response]);
-      if (window.navigator.msSaveBlob) {
-        window.navigator.msSaveBlob(blob, fname);
-      } else {
-        var url = window.URL || window.webkitURL;
-        var blobUrl = url.createObjectURL(blob);
-        var a = document.createElement('a');
-        document.body.appendChild(a);
-        a.download = u.replace('/', '_') + '_' + ymdhms + '.json';
-        a.href = blobUrl;
-        a.click();
-        document.body.removeChild(a);
-        console.log(blobUrl);
-      }
-    } else {
-      /* error */
-      /*console.log(xhr.responseText);*/
-    }
-  };
-  xhr.send();
-});
+  var base_addr = 'https://kancolle-arcade.net/ac/';
+  var api_base = base_addr + 'api/';
+  var uri_check = base_addr + '#/area';
+  var infoarray = ['Personal/basicInfo', 'Area/captureInfo', 'TcBook/info', 'EquipBook/info', 'Campaign/history', 'Campaign/info', 'Campaign/present', 'CharacterList/info', 'EquipList/info', 'Quest/info'];
+  /*    var infoarray = ['Personal/basicInfo', 'TcBook/info', 'CharacterList/info'];
+   */
+  if (window.location.href.match(new RegExp('^' + uri_check))) {
+    /*console.log('OK');*/
+    infoarray.forEach(function (u) {
+      var xhr = new XMLHttpRequest();
+      var fname = u.replace('/', '_') + '_' + ymdhms + '.json';
+      xhr.open('GET', api_base + u);
+      xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+      xhr.responseType = 'blob';
+      xhr.onload = function () {
+        if (xhr.status === 200) {
+          /* success */
+          var blob = new Blob([xhr.response]);
+          if (window.navigator.msSaveBlob) {
+            window.navigator.msSaveBlob(blob, fname);
+          } else {
+            var url = window.URL || window.webkitURL;
+            var blobUrl = url.createObjectURL(blob);
+            var a = document.createElement('a');
+            document.body.appendChild(a);
+            a.download = u.replace('/', '_') + '_' + ymdhms + '.json';
+            a.href = blobUrl;
+            a.click();
+            document.body.removeChild(a);
+            console.log(blobUrl);
+          }
+        } else {
+          /* error */
+          /*console.log(xhr.responseText);*/
+        }
+      };
+      xhr.send();
+    });
+  } else {
+    /*console.log('NG');*/
+    return;
+  }
+})();
